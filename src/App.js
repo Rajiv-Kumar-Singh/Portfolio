a018d7d53bd33f99742ad5ef4af2a35c2f00147c
Rajiv-Kumar-Singh <rv488448@gmail.com>
2022-10-08 01:37:59 +0530

    create Experience section

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import About from './components/About/About';
 // React Router 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Intro from './components/About/RouteComponents/Intro';
+import Experience from './components/About/RouteComponents/Experience';
 
 function App() {
   return (
@@ -17,10 +18,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<><Header /> <Hero /> <Works /> <About /></>} />
-          <Route exact path="/about/intro" element={<><Intro /></>} />
-          <Route exact path="/about/experience" element={<><Intro /></>} />
-          <Route exact path="/about/internships" element={<><Intro /></>} />
-          <Route exact path="/about/graduation" element={<><Intro /></>} />
+          <Route exact path="/about/intro" element={<Intro />} />
+          <Route exact path="/about/experience" element={<Experience />} />
+          <Route exact path="/about/internships" element={<Intro />} />
+          <Route exact path="/about/graduation" element={<Intro />} />
         </Routes>
       </BrowserRouter>
     </>
