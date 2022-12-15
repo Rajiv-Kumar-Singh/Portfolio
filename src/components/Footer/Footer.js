7964872f91daa67759aa1853bdb2e4cd050589cd
Rajiv-Kumar-Singh <rv488448@gmail.com>
2022-12-16 01:55:17 +0530

    Add footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { } from '@fortawesome/free-brands-svg-icons';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+
+import '../../styles/Footer/Footer.scss';
+
 const Footer = () => {
   return (
     <>
       <div className="footer">
         <div className="footer__container">
-          <p>Made with </p>
+          <p>
+            Made with <FontAwesomeIcon icon={faHeart} size="0.3x" /> by Rajiv
+          </p>
         </div>
       </div>
     </>
